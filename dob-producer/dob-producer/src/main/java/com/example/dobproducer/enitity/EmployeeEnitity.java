e8b70050a3a371c84f7ca13cf2a97bb1982c3a4f
shaileshkarpe <shaileshkarpe2018@gmail.com>
2023-01-10 23:56:56 +0530

    commited

---
+++ dob-producer/dob-producer/src/main/java/com/example/dobproducer/enitity/EmployeeEnitity.java
@@ -0,0 +1,25 @@
+package com.example.dobproducer.enitity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class EmployeeEnitity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long employeeId;
+    private String name;
+    private String dob;
+    private String email;
+    private Double salary;
+}
